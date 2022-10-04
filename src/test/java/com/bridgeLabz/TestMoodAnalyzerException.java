8c1f772eed8b1a01121790f9b0c4a5c73c69b3c3
Radhika <reddypallyradhika@gmail.com>
2022-10-04 18:47:17 +0530

    Added:Mood Analyzer Problem:TC1.2

--- src/test/java/com/bridgeLabz/TestMoodAnalyzerException.java
+++ src/test/java/com/bridgeLabz/TestMoodAnalyzerException.java
@@ -6,20 +6,28 @@ import org.junit.Test;
 public class TestMoodAnalyzerException {
 
     @Test
-    public void testMoodAnalyser_HappyOrSad()
-    {
-       // MoodAnalyzerException moodAnalyser= new MoodAnalyzerException();
-        String mood = MoodAnalyzerException.analyseMood("This is a Happy or Sad message");
+    public void testMoodAnalyser_Happy() {
+        // MoodAnalyzerException moodAnalyser= new MoodAnalyzerException();
+        String mood = MoodAnalyzerException.analyseMood("This is a Happy message");
         System.out.println(mood);
     }
-/*Given “I am in Sad Mood” message Should Return SAD
-     analyseMood method can just return SAD to pass this Test Case (TC)*/
-@Test
-public void testMoodAnalyser_Sad()
-{
-   // MoodAnalyzerException moodAnalyser= new MoodAnalyzerException();
-    String mood = MoodAnalyzerException.analyseMood("This is a sad message");
-    Assert.assertEquals(mood,"sad");
-    System.out.println(mood);
-}
+
+    /*Given “I am in Sad Mood” message Should Return SAD
+         analyseMood method can just return SAD to pass this Test Case (TC)*/
+    @Test
+    public void testMoodAnalyser_Sad() {
+        // MoodAnalyzerException moodAnalyser= new MoodAnalyzerException();
+        String mood = MoodAnalyzerException.analyseMood("This is a sad message");
+        Assert.assertEquals(mood, "sad");
+        System.out.println(mood);
+    }
+
+    //Given “I am in Any Mood” message Should Return HAPPY
+    @Test
+    public void testMoodAnalyser_HappyOrSad() {
+        // MoodAnalyzerException moodAnalyser= new MoodAnalyzerException();
+        String mood = MoodAnalyzerException.analyseMood("I am in Any Mood");
+        Assert.assertEquals(mood, "Happy");
+        System.out.println(mood);
+    }
 }
