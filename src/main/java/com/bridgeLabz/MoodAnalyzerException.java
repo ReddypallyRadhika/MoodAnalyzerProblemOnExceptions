d38a0818b9b2d41976e341895e6181540e67c6b5
Radhika <reddypallyradhika@gmail.com>
2022-10-04 18:19:02 +0530

    Added:Mood Analyzer Problem:UC1

--- src/main/java/com/bridgeLabz/MoodAnalyzerException.java
+++ src/main/java/com/bridgeLabz/MoodAnalyzerException.java
@@ -3,7 +3,7 @@ package com.bridgeLabz;
 // Given a Message, ability
 //to analyse and respond
 //Happy or Sad Mood - Create MoodAnalyser Object - Call analyseMood function with message as
-//parameter and return Happy or Sad Mood`.
+//parameter and return Happy or Sad Mood`
 public class MoodAnalyzerException {
     public static String analyseMood(String msg) {
 
