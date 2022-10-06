5691f0139b54e28136e16a1e4cae18e3de554d83
Radhika <reddypallyradhika@gmail.com>
2022-10-06 16:34:49 +0530

    Refactor the code of Mood Analyzer Problem UC1-TC1.1

--- src/main/java/com/bridgeLabz/MoodAnalyzerException.java
+++ src/main/java/com/bridgeLabz/MoodAnalyzerException.java
@@ -1,13 +1,18 @@
 package com.bridgeLabz;
-//UC1
-// Given a Message, ability
-//to analyse and respond
-//Happy or Sad Mood - Create MoodAnalyser Object - Call analyseMood function with message as
-//parameter and return Happy or Sad Mood`
-public class MoodAnalyzerException {
-    public static String analyseMood(String msg) {
 
-        if(msg.contains("sad")) {
+//UC1
+// Refactor the code to take the mood message in Constructor
+public class MoodAnalyzerException {
+    private static String message="sad";
+    public MoodAnalyzerException(String message) {
+       super();
+       MoodAnalyzerException.message =message;
+
+    }
+
+    public static String analyseMood() {
+
+        if(message.contains("sad")) {
             return "sad";
         }
         else
